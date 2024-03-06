5ee57d86a7f663dd2d3d0a8c891f4b1166f615c7
yaelo <alanyael.0000@gmail.com>
2024-03-06 10:58:49 -0600

    Rewrite with ts

---
+++ src/scripts/getAge.ts
@@ -0,0 +1,7 @@
+export function getAge(): number {
+  const today = new Date();
+  const birthdate = new Date("2002/07/02");
+  const age = today.getFullYear() - birthdate.getFullYear();
+  const isBirthdayPassed = today.getMonth() > birthdate.getMonth() || (today.getMonth() === birthdate.getMonth() && today.getDate() >= birthdate.getDate());
+  return isBirthdayPassed ? age : age - 1;
+}
