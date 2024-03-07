a7dd1932740d7eedf8375f85822df0c77e2fc309
yaelo <alanyael.0000@gmail.com>
2024-03-06 21:00:08 -0600

    Add getProfile and getRepositories to api

--- src/lib/apiGithub.ts
+++ src/lib/apiGithub.ts
@@ -1,38 +1,48 @@
-import { type APIGithubRepositories } from "src/types/api.ts";
+import { type APIGithubOwner, type APIGithubRepositories } from "src/types/api.ts";
 
-export async function getLastRepositories(): Promise<{ data: APIGithubRepositories[] | null, error: any | null }> {
-  let data: APIGithubRepositories[] = [];
-  let error: string | null = null;
-  const token = import.meta.env.PUBLIC_GITHUB_TOKEN;
+const BASE_URL = "https://api.github.com/users/yaelooo";
+const token = import.meta.env.PUBLIC_GITHUB_TOKEN;
 
+async function fetchData(url: string): Promise<{ data: any | null, error: any | null }> {
+  let data = null;
+  let error = null;
+  
   try {
     const res: Response = await fetch(
-      "https://api.github.com/users/yaelooo/repos?per_page=3&sort=updated",
+      url,
       {
         headers: {
           Authorization: `token ${token}`,
         },
         cache: "force-cache",
       }
-    );
-  
-    const statusCodes: { [status: number]: string } = {
-      400: "Bad Request",
-      401: "Unauthorized",
-      403: "Forbidden",
-      404: "Not Found",
-    };
-  
-    if (res.status >= 200 && res.status < 300) {
-      data = await res.json();
-    } else if (statusCodes[res.status]) {
-      throw new Error(statusCodes[res.status]);
-    } else {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-  } catch (err: unknown) {
-    error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
-  }
+      );
 
-  return { data, error };
-}
+      const statusCodes: { [status: number]: string } = {
+        400: "Bad Request",
+        401: "Unauthorized",
+        403: "Forbidden",
+        404: "Not Found",
+      };
+    
+      if (res.status >= 200 && res.status < 300) {
+        data = await res.json();
+      } else if (statusCodes[res.status]) {
+        throw new Error(statusCodes[res.status]);
+      } else {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+    } catch (err: unknown) {
+      error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
+    }
+    
+    return { data, error };
+  }
+  
+  export async function getProfile(): Promise<{ data: APIGithubOwner | null, error: any | null }> {
+    return await fetchData(`${BASE_URL}`);
+  }
+  
+  export async function getRepositories(perPage: number): Promise<{ data: APIGithubRepositories | null, error: any | null }> {
+    return await fetchData(`${BASE_URL}/repos?per_page=${perPage}&sort=updated`);
+  }
