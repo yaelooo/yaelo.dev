12e366d0fc3153948c583626c1b614594059a39a
yaelo <alanyael.0000@gmail.com>
2024-03-08 01:43:04 -0600

    Add getLanguages to apiGithub

--- src/lib/apiGithub.ts
+++ src/lib/apiGithub.ts
@@ -9,40 +9,43 @@ async function fetchData(url: string): Promise<{ data: any | null, error: any | 
   
   try {
     const res: Response = await fetch(
-      url,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-        cache: "force-cache",
-      }
-      );
-
-      const statusCodes: { [status: number]: string } = {
-        400: "Bad Request",
-        401: "Unauthorized",
-        403: "Forbidden",
-        404: "Not Found",
-      };
-    
-      if (res.status >= 200 && res.status < 300) {
-        data = await res.json();
-      } else if (statusCodes[res.status]) {
-        throw new Error(statusCodes[res.status]);
-      } else {
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-    } catch (err: unknown) {
-      error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
+    url,
+    {
+      headers: {
+        Authorization: `token ${token}`,
+      },
+      cache: "force-cache",
     }
-    
-    return { data, error };
-  }
+    );
+
+    const statusCodes: { [status: number]: string } = {
+      400: "Bad Request",
+      401: "Unauthorized",
+      403: "Forbidden",
+      404: "Not Found",
+    };
   
-  export async function getProfile(): Promise<{ data: APIGithubOwner | null, error: any | null }> {
-    return await fetchData(`${BASE_URL}`);
+    if (res.status >= 200 && res.status < 300) {
+      data = await res.json();
+    } else if (statusCodes[res.status]) {
+      throw new Error(statusCodes[res.status]);
+    } else {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+  } catch (err: unknown) {
+    error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
   }
+  return { data, error };
+}
   
-  export async function getRepositories(perPage: number): Promise<{ data: APIGithubRepositories | null, error: any | null }> {
-    return await fetchData(`${BASE_URL}/repos?per_page=${perPage}&sort=updated`);
-  }
+export async function getProfile(): Promise<{ data: APIGithubOwner | null, error: any | null }> {
+  return await fetchData(`${BASE_URL}`);
+}
+
+export async function getRepositories(perPage: number): Promise<{ data: APIGithubRepositories | null, error: any | null }> {
+  return await fetchData(`${BASE_URL}/repos?per_page=${perPage}&sort=updated`);
+}
+
+export async function getLanguages(repo: string) {
+  return await fetchData(`https://api.github.com/repos/yaelooo/${repo}/languages`);
+}
