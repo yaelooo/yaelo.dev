3fdb520c2e3c8c8ac20713cf29288674e61293f5
yaelo <alanyael.0000@gmail.com>
2024-03-11 15:50:13 -0600

    Refactor and update GitHub API functions

--- src/lib/apiGithub.ts
+++ src/lib/apiGithub.ts
@@ -1,55 +1,87 @@
 import { type APIGithubOwner, type APIGithubRepositories } from "src/types/api.ts";
 
-const BASE_URL = "https://api.github.com/users/yaelooo";
+const username = "yaelooo";
+const url_api = `https://api.github.com`;
+const url_raw = `https://raw.githubusercontent.com`;
 const token = import.meta.env.PUBLIC_GITHUB_TOKEN;
 
-async function fetchData(url: string): Promise<{ data: any | null, error: any | null }> {
-  let data = null;
-  let error = null;
-  
-  try {
-    const res: Response = await fetch(
-    url,
-    {
-      headers: {
-        Authorization: `token ${token}`,
-      },
-      cache: "force-cache",
-    }
-    );
+function handleStatusCodes(res: Response): void {
+  const statusCodes: { [status: number]: string } = {
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+  };
 
-    const statusCodes: { [status: number]: string } = {
-      400: "Bad Request",
-      401: "Unauthorized",
-      403: "Forbidden",
-      404: "Not Found",
-    };
-  
-    if (res.status >= 200 && res.status < 300) {
-      data = await res.json();
-    } else if (statusCodes[res.status]) {
+  if (res.status < 200 || res.status >= 300) {
+    if (statusCodes[res.status]) {
       throw new Error(statusCodes[res.status]);
     } else {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
+  }
+}
+
+async function fetchDataFromAPI(url: string): Promise<{ data: any | null, error: any | null }> {
+  let data = null;
+  let error = null;
+
+  try {
+    const res: Response = await fetch(
+      url,
+      {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+        cache: "force-cache",
+      }
+    );
+
+    handleStatusCodes(res);
+    data = await res.json();
   } catch (err: unknown) {
     error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
   }
+
+  return { data, error };
+}
+
+async function fetchDataFromRaw(url: string): Promise<{ data: string | null, error: any | null }> {
+  let data = null;
+  let error = null;
+
+  try {
+    const res: Response = await fetch(url);
+
+    handleStatusCodes(res);
+    data = await res.text();
+  } catch (err: unknown) {
+    error = "Error fetching data: " + (err instanceof Error ? err.message : String(err));
+  }
+
   return { data, error };
 }
   
 export async function getProfile(): Promise<{ data: APIGithubOwner | null, error: any | null }> {
-  return await fetchData(`${BASE_URL}`);
+  return await fetchDataFromAPI(`${url_api}/users/${username}`);
 }
 
 export async function getRepositories(perPage: number): Promise<{ data: APIGithubRepositories | null, error: any | null }> {
-  return await fetchData(`${BASE_URL}/repos?per_page=${perPage}&sort=updated`);
+  return await fetchDataFromAPI(`${url_api}/users/${username}/repos?per_page=${perPage}&sort=updated`);
 }
 
 export async function getRepository(repo: string): Promise<{ data: APIGithubRepositories | null, error: any | null }> {
-  return await fetchData(`https://api.github.com/repos/yaelooo/${repo}`);
+  return await fetchDataFromAPI(`${url_api}/repos/${username}/${repo}`);
 }
 
-export async function getLanguages(repo: string) {
-  return await fetchData(`https://api.github.com/repos/yaelooo/${repo}/languages`);
+export async function getLanguages(repo: string): Promise<{ data: { [key: string]: number } | null, error: any | null }> {
+  return await fetchDataFromAPI(`${url_api}/repos/${username}/${repo}/languages`);
+}
+
+export async function getReadme(repo: string) {
+  return await fetchDataFromRaw(`${url_raw}/${username}/${repo}/main/README.md`);
+}
+
+export function getProjectImageUrl(repo: string): string {
+  return `${url_raw}/${username}/${repo}/main/.ignore/${repo}.webp`;
 }
